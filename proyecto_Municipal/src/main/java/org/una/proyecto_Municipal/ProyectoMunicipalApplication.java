7cbced5476f00b35516a58b1f0316acace25e221
Robotooo <50878739+Robotooo@users.noreply.github.com>
2021-10-07 14:38:08 -0600

    Inicio proyecto
    Inicializacion del proyecto

---
+++ proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/ProyectoMunicipalApplication.java
@@ -0,0 +1,13 @@
+package org.una.proyecto_Municipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMunicipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMunicipalApplication.class, args);
+	}
+
+}
---
+++ proyecto_Municipal/src/test/java/org/una/proyecto_Municipal/ProyectoMunicipalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.proyecto_Municipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMunicipalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
