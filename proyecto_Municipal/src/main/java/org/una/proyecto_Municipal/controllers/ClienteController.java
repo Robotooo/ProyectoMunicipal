205805b1fe5c437b243539e7f833d5f1dfabffd0
Henry <henrygova@hotmail.com>
2021-10-09 00:17:41 -0600

    libraries and packages

--- proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
+++ proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package org.una.proyecto_Municipal.controllers;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import io.swagger.annotations.Api;
