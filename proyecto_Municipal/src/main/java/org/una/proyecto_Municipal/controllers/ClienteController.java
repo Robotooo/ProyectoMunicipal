cdec41349b9df43ebc42e82ba62cc62da724fbfc
Henry <henrygova@hotmail.com>
2021-10-09 00:47:25 -0600

    Revert "Swagger y cambios pequeños"
    This reverts commit 15820f15daf8f3bc101c4855924f76801d3dbdd2.

--- proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
+++ proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
@@ -1,17 +1,12 @@
 package org.una.proyecto_Municipal.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-
-@RestController
-@RequestMapping("/clientes")
-@Api(tags = {"Clientes"})
+//@RestController
+//@RequestMapping("/clientes")
+//@Api(tags = {"Clientes"})
 
 public class ClienteController {
 
 //    @Autowired
-//    private IClienteService = clienteService;
+//    private IClienteService= clienteService;
 
 }
