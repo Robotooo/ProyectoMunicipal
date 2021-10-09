972770b5c5e8cc75592c2ed94542fcfb4228ab46
Henry <henrygova@hotmail.com>
2021-10-09 00:44:00 -0600

    Revert "libraries and packages"
    This reverts commit 205805b1fe5c437b243539e7f833d5f1dfabffd0.

--- proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
+++ proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/ClienteController.java
@@ -1,6 +1,5 @@
 package org.una.proyecto_Municipal.controllers;
 
-import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import io.swagger.annotations.Api;
