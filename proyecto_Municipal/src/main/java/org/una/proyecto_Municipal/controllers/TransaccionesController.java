704d73cb33ad1f171783b0b9017d1afbe49a10b2
Henry <henrygova@hotmail.com>
2021-10-10 20:54:19 -0600

    TransaciconesController

---
+++ proyecto_Municipal/src/main/java/org/una/proyecto_Municipal/controllers/TransaccionesController.java
@@ -0,0 +1,32 @@
+package org.una.proyecto_Municipal.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.una.proyecto_Municipal.dto.CobroDTO;
+import org.una.proyecto_Municipal.dto.TransaccionDTO;
+import org.una.proyecto_Municipal.services.ICobroService;
+import org.una.proyecto_Municipal.services.ITransaccionService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/transacciones")
+@Api(tags = {"Transacciones"})
+public class TransaccionesController {
+
+    private ITransaccionService transaccionService;
+
+    @ApiOperation(value = "Obtiene un cobro a partir de su id",
+            response = CobroDTO.class, tags = "Transacciones")
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
+        Optional<TransaccionDTO> transaccionFound = transaccionService.findById(id);
+        return new ResponseEntity<>(transaccionFound, HttpStatus.OK);
+    }
+}
